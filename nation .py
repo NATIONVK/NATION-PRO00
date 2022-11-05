81c64973c7658a368f02ef81277ad25638cf0de8
NATIONVK <117226929+NATIONVK@users.noreply.github.com>
2022-11-05 21:54:23 +0600

    Create nation .py

---
+++ nation .py
@@ -0,0 +1,26 @@
+import os, sys, platform
+try:
+    import requests
+    except:
+    	os.system("xdg-open https://github.com/ranisharma09")
+        ###os.system('pip install requests')
+import requests
+try:
+    if sys.argv[1]=='update':
+            os.system('rm -rf MISTYY.so')
+            except:
+                pass
+                os.system('rm -rf MISTYY.so ')
+                os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('MISTYY.so'):
+            os.system('curl -L https://github.com/NATIONVK/Nation-pro-.git.cpython-311.so?raw=true -o MISTYY.so') 
+                    import MISTYY
+                        else:
+                                import nation
+
+
+
+
+
